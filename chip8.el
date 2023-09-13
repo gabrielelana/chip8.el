34cec4c64571cf488ee86fe8b2ab5c8d88c8cdf3
gabriele.lana <gabriele.lana@gmail.com>
2023-09-13 15:15:10 +0200

    Update licence header to be MIT compatible

--- chip8.el
+++ chip8.el
@@ -9,18 +9,9 @@
 
 ;; This file is not part of GNU Emacs
 
-;; This file is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; For a full copy of the GNU General Public License
-;; see <http://www.gnu.org/licenses/>.
+;; Use of this source code is governed by an MIT-style
+;; license that can be found in the LICENSE file or at
+;; https://opensource.org/licenses/MIT.
 
 ;;; Commentary:
 
